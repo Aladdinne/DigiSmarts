0333c0ba673428b70a80258b55ff24f2a8113dcf
Aladdinne <aladin.hammouda@esprit.tn>
2023-07-09 01:43:25 +0100

    now

---
+++ src/main/java/com/stage/digibackend/controllers/SensorController.java
@@ -0,0 +1,58 @@
+package com.stage.digibackend.controllers;
+
+import com.stage.digibackend.Collections.Device;
+import com.stage.digibackend.Collections.Sensor;
+import com.stage.digibackend.services.ISensorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+@RequestMapping("/sensor")
+@CrossOrigin(origins = "*")
+public class SensorController {
+
+    @Autowired
+    ISensorService iSensorService ;
+
+    @PostMapping("/addSensor")
+    String addSensor(@RequestBody Sensor sensor){
+return iSensorService.addSensor(sensor);
+    }
+
+    @PutMapping("/updateSensor/{idSensor}")
+    String updateSensor(@RequestBody Sensor sensor, @PathVariable String idSensor)
+    {
+        return iSensorService.updateSensor(sensor,idSensor);
+    }
+
+    @DeleteMapping("/deleteSensor/{sensorId}")
+    String deleteSensor(@PathVariable String sensorId){
+return iSensorService.deleteSensor(sensorId);
+    }
+
+    @GetMapping("/getSensorById/{sensorId}")
+    Sensor getSensor(@PathVariable String sensorId)
+    {
+        return iSensorService.getSensor(sensorId);
+    }
+
+    @GetMapping("/getAllSensorsById")
+    String getAllSensors(@RequestBody List<String> sensorIds)
+    {
+        return iSensorService.getAllSensors(sensorIds);
+    }
+
+    @GetMapping("/getAllSensors")
+    List<Sensor> getAllSensors(){
+        return iSensorService.getAllSensors();
+}
+
+    @GetMapping("/getAllSensorsSignal420")
+    public List<Sensor> getAllSensorsSignal420() {
+        return iSensorService.getAllSensorsSignal420();
+    }
+
+}
